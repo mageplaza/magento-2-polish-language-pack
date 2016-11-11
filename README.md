8e4855b756737419caf0d31b2cbe034c71921b79
Sam <sam@mageplaza.com>
2016-11-11 06:46:44 -0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Magento 2 Polish Language Pack
+
+Read more [Magento 2 Polish Language Pack](https://www.mageplaza.com/magento-2-polish-language-pack.html)
+
+## How to Install
+
+
+### via Composer
+
+**Install Polish pack**:
+
+```
+composer require mageplaza/magento-2-polish-language-pack:*
+php bin/magento cache:clean
+php bin/magento setup:static-content:deploy pl-pl
+
+```
+
+
+Ref: https://www.mageplaza.com/magento-2-polish-language-pack.html
---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     Mageplaza_Polish package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://www.mageplaza.com/LICENSE.txt
+ *
+ *                     @category  Mageplaza
+ *                     @package   Mageplaza_Polish
+ *                     @copyright Copyright (c) 2016
+ *                     @license   https://www.mageplaza.com/LICENSE.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'mageplaza_pl_pl',
+    __DIR__
+);
